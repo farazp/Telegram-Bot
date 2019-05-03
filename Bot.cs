dae51b6ee4a518b16d71bf75761d239832e6655d
Faraz Pourafshin <faraz_tayyari@yahoo.com>
2019-05-04 01:03:29 +0430

    Add files via upload

---
+++ Bot.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using WhoisMasterBot.Models.Commands;
+
+namespace WhoisMasterBot.Models
+{
+    public class Bot
+    {
+        public Bot(IOptions<BotConfiguration> _config)
+        {
+            //_config = config.Value;
+            //_configuration.GetSection("BotConfiguration");
+            config = _config;
+            botConfiguration = _config.Value;
+        }
+        private static TelegramBotClient botClient;
+        private static List<Command> commandsList;
+        public static IReadOnlyList<Command> Commands => commandsList.AsReadOnly();
+
+        private readonly IOptions<BotConfiguration> config;
+        private BotConfiguration botConfiguration = new BotConfiguration();
+
+        public static async Task<TelegramBotClient> GetBotClientAsync()
+        {
+            if (botClient != null)
+            {
+                return botClient;
+            }
+
+            //var botConfiguration = new BotConfiguration();
+            //_Configuration.GetSection("BotConfiguration").Bind(botConfiguration);
+            commandsList = new List<Command>();
+            commandsList.Add(new StartCommand());
+            //TODO: Add more commands
+            var _token = Startup.StaticConfig.GetSection("BotConfiguration:BotToken").Value;
+            var _url = Startup.StaticConfig.GetSection("BotConfiguration:Url").Value;
+
+            botClient = new TelegramBotClient(token: _token);
+            botClient.Timeout = new TimeSpan(0, 0, 360);
+            string hook = string.Format(_url, "api/DomainWhois");
+            await botClient.SetWebhookAsync(hook);
+
+            return botClient;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WhoisMasterBot.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WhoisMasterBot.Models;
+using WhoisMasterBot.Services;
+using System.IO;
+
+namespace WhoisMasterBot
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            StaticConfig = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public static IConfiguration StaticConfig { get; private set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddDefaultUI(UIFramework.Bootstrap4)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddTransient<IWhoIs, Whois>();
+            services.AddTransient<IAddressHelper, AddressHelper>();
+            services.Configure<BotConfiguration>(options => Configuration.GetSection("BotConfiguration").Bind(options));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext ctx)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseCors();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            DbInitializer.Initialize(ctx, env);
+
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+
+
+            //Bot Configurations
+            Bot.GetBotClientAsync().Wait();
+        }
+    }
+}
